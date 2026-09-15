1ea8b8152dabb07b84ed4a8d313bb69dbadfaf38
epi618 <1006333969@qq.com>
2026-09-15 09:27:20 +0800

    Update OpenWrt DIY script for configuration changes
    Modify default IP, remove unnecessary plugins, and set Argon theme.
    
    Signed-off-by: epi618 <1006333969@qq.com>

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
+# 1. 修改默认后台登录 IP 为 192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 2. 物理删除不需要的冗余插件目录（保留 autoreboot，其余彻底剔除）
+rm -rf feeds/luci/applications/luci-app-vsftpd
+rm -rf feeds/luci/applications/luci-app-vlmcsd
+rm -rf feeds/luci/applications/luci-app-upnp
+rm -rf feeds/luci/applications/luci-app-arpbind
+rm -rf feeds/packages/net/vsftpd
+rm -rf feeds/packages/net/vlmcsd
+rm -rf feeds/packages/net/miniupnpd
+
+# 3. 设置默认主题为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
