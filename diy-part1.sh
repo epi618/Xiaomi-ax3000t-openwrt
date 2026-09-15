8846989400b4a4664aaa69dd7a5c42093031e12c
epi618 <wdh1006333969@gmail.com>
2026-09-15 16:22:18 +0800

    Update diy-part1.sh
    Signed-off-by: epi618 <wdh1006333969@gmail.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 清理所有历史遗留的 passwall 源
+# 清除所有历史遗留 passwall 相关配置
 sed -i '/passwall/d' feeds.conf.default
 
-# 注入核心依赖包仓库（使用官方默认 main 分支）
+# 注入核心依赖包（使用 main 分支）
 echo 'src-git passwallpkg https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
