03996f9cc24b4f44f00e3a674d3c725a4c5c0887
epi618 <wdh1006333969@gmail.com>
2026-09-16 14:59:42 +0800

    Update diy-part1.sh
    Signed-off-by: epi618 <wdh1006333969@gmail.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,6 +6,3 @@ sed -i '/passwall/d' feeds.conf.default
 
 # 2. 注入 PassWall 核心依赖包仓库 (采用官方 main 分支)
 echo 'src-git passwallpkg https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-
-# 3. 注入 Lucky 官方客户端 feed
-echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
